e8ab2a4309e660aebb217ee973c67e0e900bad5b
noguchi_kohei <kohei@PC.local>
2017-03-20 05:54:58 +0900

    dynamodb用のentity定義作成

--- src/main/java/jp/ddd/server/adapter/gateway/dynamodb/table/RoomUserDyn.java
+++ src/main/java/jp/ddd/server/adapter/gateway/dynamodb/table/RoomUserDyn.java
@@ -1,7 +1,6 @@
 package jp.ddd.server.adapter.gateway.dynamodb.table;
 
-
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +12,17 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Data
 @DynamoDBTable(tableName = "ds_room_user")
-public class RoomUserDyn implements Serializable{
+public class RoomUserDyn implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @DynamoDBHashKey(attributeName = "user_id")
+    @DynamoDBIndexHashKey(globalSecondaryIndexName = "idx_uid_lastat")
+    private String userId;
+    @DynamoDBAttribute(attributeName = "room_id")
+    private String roomId;
+    @DynamoDBAttribute(attributeName = "last_room_message_at")
+    @DynamoDBIndexRangeKey(globalSecondaryIndexName = "idx_uid_lastat")
+    private String lastRoomMessageAt;
+    @DynamoDBAttribute(attributeName = "join_at")
+    private String joinAt;
 }
