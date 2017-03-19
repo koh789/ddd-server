76f3dd403114af676c95f2bc16dd34715cc0c0bd
noguchi_kohei <kohei@PC.local>
2017-03-20 05:32:53 +0900

    spring-data-dynamo接続成功！

--- src/main/java/jp/ddd/server/usecase/gateway/dynamodb/UserDynGateway.java
+++ src/main/java/jp/ddd/server/usecase/gateway/dynamodb/UserDynGateway.java
@@ -3,6 +3,7 @@ package jp.ddd.server.usecase.gateway.dynamodb;
 import jp.ddd.server.adapter.gateway.dynamodb.custom.UserDynGatewayCtm;
 import jp.ddd.server.adapter.gateway.dynamodb.table.UserDyn;
 import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
+import org.socialsignin.spring.data.dynamodb.repository.DynamoDBPagingAndSortingRepository;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by noguchi_kohei 
  */
 @EnableScan
-public interface UserDynGateway extends CrudRepository<UserDyn, Integer>, UserDynGatewayCtm {
+public interface UserDynGateway extends DynamoDBPagingAndSortingRepository<UserDyn, String>, UserDynGatewayCtm {
 }
